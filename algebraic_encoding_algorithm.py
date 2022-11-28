2a06b0adea8582c55c63bfa7f2e83bbbf07f50e8
Kirill <59337566+LastFaust@users.noreply.github.com>
2022-11-28 18:39:00 +0300

    Update algebraic_encoding_algorithm.py

--- algebraic_encoding_algorithm.py
+++ algebraic_encoding_algorithm.py
@@ -1,1 +1,8 @@
-//алгоритм)))
+import os
+
+print("Input what u want to do. If U want to code text, press 1, else U want decode - 2")
+choice = int(input())
+if (choice == 1):
+    print(choice)
+else:
+    print(choice)
