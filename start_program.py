9a6381bf3fce2a8e65484165c95c9d01d0dd6832
Kirill <59337566+LastFaust@users.noreply.github.com>
2022-11-28 18:46:27 +0300

    Update and rename algebraic_encoding_algorithm.py to start_program.py

--- start_program.py
+++ start_program.py
@@ -3,6 +3,6 @@ import os
 print("Input what u want to do. If U want to code text, press 1, else U want decode - 2")
 choice = int(input())
 if (choice == 1):
-    print(choice)
+    os.system('code_algorithm.py')
 else:
-    print(choice)
+    print('decode_algorithm.py')
