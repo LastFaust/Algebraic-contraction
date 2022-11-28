c4d96ef8c408f2eb27d04764ced2416f62bc4d3e
Kirill <59337566+LastFaust@users.noreply.github.com>
2022-11-28 19:09:55 +0300

    Update code_algorithm.py

--- code_algorithm.py
+++ code_algorithm.py
@@ -1,1 +1,17 @@
+import os
 
+sum_slovar = 0
+with open('in.txt', 'r') as fp:
+    text_sum = 0
+    text = fp.read(1)
+    dictionary = {}
+    while text:
+        text_sum += 1
+        if dictionary.get(text) == None:
+            dictionary.update({text: 1})
+        else:
+            dictionary[text] = dictionary[text] + 1
+
+        text = fp.read(1)
+        print(text)
+    print(dictionary)
