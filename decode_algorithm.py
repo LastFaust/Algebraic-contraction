926fbff2b59be3010c4a596bb87802ac0ec089c4
Kirill <59337566+LastFaust@users.noreply.github.com>
2022-11-29 05:10:25 +0300

    Update decode_algorithm.py

--- decode_algorithm.py
+++ decode_algorithm.py
@@ -1,1 +1,11 @@
+import os
+global write_bit
+global bit_len
 
+with open("output.txt", "rb") as file:
+    text = ord(file.read(1))
+    print(text)
+    dictionary = {}
+    for i in range(text):
+        key_word = file.read(1).decode('ascii')
+        # print(key_word)
