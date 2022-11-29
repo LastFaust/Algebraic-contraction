20e4ec851a5b183471f537487ccc9f27598017d8
Kirill <59337566+LastFaust@users.noreply.github.com>
2022-11-29 07:36:15 +0300

    Update decode_algorithm.py

--- decode_algorithm.py
+++ decode_algorithm.py
@@ -14,7 +14,7 @@ def in_put_bit(file):
     if (bit_len == 0):
         bit = file.read(1)
         read_bit = int.from_bytes(bit, "little")
-         if (bit == b""):
+        if (bit == b""):
             rubbish_bit += 1
             read_bit = 255
             if (rubbish_bit > 14):
@@ -42,7 +42,6 @@ with open("coded_text.txt", "rb") as coded_text_file:
     # print(work_interval)
 
     # алгоритм раскодирования
-    # алгоритм раскодирования
     with open("output.txt", "wb+") as output_file:
         low_interval = 0
         high_interval = (1 << 16) - 1
@@ -52,6 +51,31 @@ with open("coded_text.txt", "rb") as coded_text_file:
         half_q = first_q * 2
         third_q = first_q * 3
         val = 0
-        
+
         for iterator in range(16):
             k = in_put_bit(output_file)
+            val += val + k
+        while True:
+            fraq = int((val - low_interval + 1) * delete - 1) / diff
+            j = 1
+            while work_interval[j] <= fraq:
+                j += 1
+            high_interval = int(low_interval + work_interval[j] * diff / delete - 1)
+            low_interval = int(low_interval + work_interval[j-1] * diff / delete)
+
+            while True:
+                if high_interval < half_q:
+                    pass
+                elif low_interval >= half_q:
+                    low_interval -= half_q
+                    high_interval -= half_q
+                    val -= half_q
+                elif low_interval >= first_q and high_interval < third_q:
+                    low_interval -= first_q
+                    high_interval -= first_q
+                    val -= first_q
+                else:
+                    break
+                output_file.write(list(dictionary.keys())[j-2].encode('ascii'))
+                diff = high_interval - low_interval + 1
+output_file.close()
