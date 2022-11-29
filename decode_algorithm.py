1d2bb19ff8ebbfcdb57f5d899702a638513c5c4b
Kirill <59337566+LastFaust@users.noreply.github.com>
2022-11-29 11:25:16 +0300

    Update decode_algorithm.py

--- decode_algorithm.py
+++ decode_algorithm.py
@@ -1,51 +1,53 @@
-import os
-
 global read_bit
 global bit_len
-global rubbish_bit
-rubbish_bit = 0
-bit_len = 0
+global garbage_bit
 read_bit = 0
+bit_len = 0
+garbage_bit = 0
 
-def in_put_bit(file):
+
+def in_put_bit(input_file):
     global read_bit
     global bit_len
-    global rubbish_bit
-    if (bit_len == 0):
-        bit = file.read(1)
-        read_bit = int.from_bytes(bit, "little")
-        if (bit == b""):
-            rubbish_bit += 1
+    global garbage_bit
+    if bit_len == 0:
+        sid_bit = input_file.read(1)
+        read_bit = int.from_bytes(sid_bit, "little")
+        if sid_bit == b"":
+            garbage_bit += 1
             read_bit = 255
-            if (rubbish_bit > 14):
+            if garbage_bit > 14:
+                print("File hasn't been decoded")
                 exit(1)
-
         bit_len = 8
+
     t = read_bit & 1
     read_bit >>= 1
     bit_len -= 1
     return t
+
 
 with open("coded_text.txt", "rb") as coded_text_file:
     text = ord(coded_text_file.read(1))
     # print(text)
     dictionary = {}
     for i in range(text):
-        key_hadder = coded_text_file.read(1).decode('ascii')
-        # print(key_word)
-        value_hadder = int.from_bytes(coded_text_file.read(4), "little")
-        dictionary[key_hadder] = value_hadder
+        key_header = coded_text_file.read(1).decode('utf-8')
+        #print(key_header)
+        value_header = int.from_bytes(coded_text_file.read(4), "little")
+        #print(value_header)
+        dictionary[key_header] = value_header
     # print(dictionary)
+    print("     File read successfully")
 
     work_interval = [0, 1]
     for i in dictionary:
         work_interval.append(dictionary[i] + work_interval[-1])
     # print(work_interval)
 
-    # алгоритм раскодирования
     with open("output.txt", "wb+") as output_file:
         low_interval = 0
-        high_interval = (1 << 16) - 1
+        high_interval = (1 << 16) - 1  # 2^16
         delete = work_interval[-1]
         diff = high_interval - low_interval + 1
         first_q = int(int(high_interval + 1) / 4)
@@ -53,16 +55,16 @@ with open("coded_text.txt", "rb") as coded_text_file:
         third_q = first_q * 3
         val = 0
 
-        for iterator in range(16):
-            k = in_put_bit(output_file)
+        for i in range(16):
+            k = in_put_bit(coded_text_file)
             val += val + k
         while True:
-            fraq = int((val - low_interval + 1) * delete - 1) / diff
+            freq = int(((val - low_interval + 1) * delete - 1) / diff)
             j = 1
-            while work_interval[j] <= fraq:
+            while work_interval[j] <= freq:
                 j += 1
             high_interval = int(low_interval + work_interval[j] * diff / delete - 1)
-            low_interval = int(low_interval + work_interval[j-1] * diff / delete)
+            low_interval = int(low_interval + work_interval[j - 1] * diff / delete)
 
             while True:
                 if high_interval < half_q:
@@ -81,9 +83,11 @@ with open("coded_text.txt", "rb") as coded_text_file:
                 high_interval += high_interval + 1
                 k = in_put_bit(coded_text_file)
                 val += val + k
-                if (j == 1):
-                    break
-                output_file.write(list(dictionary.keys())[j-2].encode('ascii'))
-                diff = high_interval - low_interval + 1
+
+            if j == 1:
+                break
+            output_file.write(list(dictionary.keys())[j - 2].encode('utf-8'))
+            diff = high_interval - low_interval + 1
+
 output_file.close()
-print("File has been decoded")
+print("U file has been successful decoded")
